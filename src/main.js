79342dbdb3b77e449fb77aa1d346c90e82cb0f7d
Kelver <kelver_kmv@hotmail.com>
2022-09-30 17:27:08 -0300

    fix: Change css to scss to add color.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import 'bootstrap/scss/bootstrap.scss'
-import './style.css'
+import './style.scss'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
@@ -8,11 +8,13 @@ import {library} from "@fortawesome/fontawesome-svg-core";
 import {fas} from "@fortawesome/free-solid-svg-icons";
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import axios from './plugins/axios'
 
 library.add(fas, far, fab)
 
 const pinia = createPinia()
 const app = createApp(App)
+app.config.globalProperties.$http = axios;
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(pinia)
